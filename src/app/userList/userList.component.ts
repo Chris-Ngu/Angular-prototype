ef5484a01d8223d699337182e53f419c4ef78ba5
Christopher Nguyen <Chris_ngu@pm.me>
2021-02-08 09:09:38 -0600

    adding states and input to userList component

--- src/app/userList/userList.component.ts
+++ src/app/userList/userList.component.ts
@@ -7,6 +7,9 @@ import { Component } from "@angular/core";
 })
 
 export class userListComponent {
+    newUserName: string = "";
+    newAge: number = -1;
+
     // testUsers = ['Jack', "John", "Jim"];
     users = [
         {
@@ -24,5 +27,31 @@ export class userListComponent {
             name: "jim",
             age: 29
         }
-    ]
+    ];
+
+    removeUser = (userid: number): void => {
+        this.users = this.users.filter(user => user.id !== userid);
+    }
+
+    setNewUsername = (nameEvent: Event | null): void => {
+        this.newUserName = (nameEvent!.target as HTMLInputElement).value;
+    }
+
+    setNewAge = (ageEvent: Event | null): void => {
+        this.newAge = parseInt((ageEvent!.target as HTMLInputElement).value);
+    }
+
+    addUser = (): void => {
+        if (this.newUserName.length === 0 || this.newAge === -1) {
+            return;
+        }
+        const uniqueID = Math.random()
+        const newUser = {
+            id: uniqueID,
+            name: this.newUserName,
+            age: this.newAge
+        };
+        this.users.push(newUser);
+        this.newUserName = "";
+    }
 }
